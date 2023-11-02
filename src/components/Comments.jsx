e03ed8c432d80108b41b594cbc658a3e7d5a3200
unknown <panhareach.phat@student.cadt.edu.kh>
2023-11-02 14:37:57 +0700

    fixes typo

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -6,7 +6,7 @@ function Comments() {
     // script.src = 'https://utteranc.es/client.js';
     // script.async = true;
     script.setAttribute('src', 'https://utteranc.es/client.js')
-    script.setAttribute('repo', 'Re4ch-Jay/phatpanhreach'); // Replace with your repository name
+    script.setAttribute('repo', 'Re4ch-Jay/phatpanhareach'); // Replace with your repository name
     script.setAttribute('issue-term', 'url');
     script.setAttribute('theme', 'github-dark');
     script.setAttribute('async', true);
