58dd4be7e416907e6315052a1a96f6cbfa7b0895
unknown <panhareach.phat@student.cadt.edu.kh>
2023-11-25 11:40:53 +0700

    fix og

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <meta property="og:title" content="Phat Panhareach - Portfolio" />
       <meta property="og:description" content="I'm an enthusiastic learner who thrives on exploring novel technologies. My journey has led me to captivating projects, and I'm an active creator and contributor to open-source initiatives."/>
       <meta property="og:image" content="/banner.png" />
+      <meta property="og:url" content="https://phatpanhareach.vercel.app/"/>
+      <meta property="og:type" content="website"/>
     </Helmet>
     <App />
   </React.StrictMode>,
