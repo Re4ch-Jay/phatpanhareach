ca22e2ce9ac532d5232376c5175c1fe50946ea04
unknown <panhareach.phat@student.cadt.edu.kh>
2023-11-25 10:11:50 +0700

    Fix meta tag

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Helmet } from 'react-helmet';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Helmet>
+      <meta name="description" content="I'm an enthusiastic learner who thrives on exploring novel technologies. My journey has led me to captivating projects, and I'm an active creator and contributor to open-source initiatives."/>
+      <meta property="og:image" content="/banner.png" />
+    </Helmet>
     <App />
   </React.StrictMode>,
 )
