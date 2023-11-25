92c8dc648faf54032124b5f612643c0c5f2eea2f
unknown <panhareach.phat@student.cadt.edu.kh>
2023-11-25 14:12:52 +0700

    update seo

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <Helmet prioritizeSeoTags>
-        <title>Phat Panhareach - Portfolio</title>
-        <meta property="og:title" content="Phat Panhareach - Portfolio" />
-        <meta property="og:description" content="I'm an enthusiastic learner who thrives on exploring novel technologies. My journey has led me to captivating projects, and I'm an active creator and contributor to open-source initiatives."/>
-        <meta property="og:image" content="/banner.png" />
-        <meta property="og:url" content="https://phatpanhareach.vercel.app/"/>
-        <meta property="og:type" content="website"/>
-      </Helmet>
     <App/>
     </HelmetProvider>
   </React.StrictMode>,
--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -24,13 +24,20 @@ export default function BlogDetail() {
 
   return (
     <>
+      <Helmet prioritizeSeoTags>
+        <title>{matchingBlog.title}</title>
+        <meta property="og:title" content={matchingBlog.title} />
+        <meta property="og:description" content={matchingBlog.description} />
+        <meta name='description' content={matchingBlog.description} />
+        <meta property="og:image" content={matchingBlog.image === "" ? "/banner.png" : matchingBlog.image} />
+      </Helmet>
      {matchingBlog && (
         <Helmet prioritizeSeoTags>
           <title>{matchingBlog.title}</title>
+          <meta property="og:title" content={matchingBlog.title} />
           <meta property="og:description" content={matchingBlog.description} />
           <meta name='description' content={matchingBlog.description} />
           <meta property="og:image" content={matchingBlog.image === "" ? "/banner.png" : matchingBlog.image} />
-          <meta property="og:title" content={matchingBlog.title} />
         </Helmet>
       )}
      
